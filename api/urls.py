184a21bc761b5c3778d7310c95cbaadaedee60c4
Steven <uribgp@gmail.com>
2021-01-12 14:47:03 -0700

    room

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 urlpatterns = [
     # as_view gives the view for it
@@ -7,5 +7,6 @@ urlpatterns = [
     path("create-room", CreateRoomView.as_view()),
     path('get-room', GetRoom.as_view()),
     path("join-room", JoinRoom.as_view()),
-    path('user-in-room', UserInRoom.as_view())
+    path('user-in-room', UserInRoom.as_view()),
+    path('leave-room', LeaveRoom.as_view())
 ]
